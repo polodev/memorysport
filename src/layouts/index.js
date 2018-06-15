15134fda75719452f68a38e35a62ee48e4be8957
polodev <polodev10@gmail.com>
2018-06-15 20:36:04 +0600

    added home page link at bottom

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import Link from 'gatsby-link'
 
 export default ({ children }) =>
-  <div style={{ margin: `0 20px`, padding: `1.25rem 1rem` }}>
+  <div style={{ margin: `10px 20px`, padding: `1.25rem 1rem` }}>
     <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-      <h3 style={{marginBottom: 20}}>Memory sport </h3>
+      <h3 style={{marginBottom: 20, textAlign: 'center'}}>Memory sport </h3>
     </Link>
       {children()}
+    <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+      <h3 style={{marginBottom: 20, textAlign: 'center'}}>Memory sport </h3>
+    </Link>
   </div>
