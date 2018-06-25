23f778b1f936bd198d35b14a5dbccb07cd4cb4ee
polodev <polodev10@gmail.com>
2018-06-25 09:32:29 +0600

    Updates

--- path---detailloci-a0e39f21c11f6a62c5ab.js
+++ path---detailloci-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([77171638734287],{337:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([77171638734287],{339:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---detailloci-a0e39f21c11f6a62c5ab.js.map
--- path---major-a0e39f21c11f6a62c5ab.js
+++ path---major-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([34873835975387],{340:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([34873835975387],{342:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---major-a0e39f21c11f6a62c5ab.js.map
