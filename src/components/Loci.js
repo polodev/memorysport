98af0045512c2e29812731c2604ab6433f98115c
polodev <polodev10@gmail.com>
2018-06-29 23:09:57 +0600

    changed in component

--- src/components/Loci.js
+++ src/components/Loci.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from '../utils/style.module.css'
 
-export default ({pageTitle, arr, id='test'}) => 
-<div>
-  <h1 id={id} className={styles.pageHeader}>
-    <a href='#'>{pageTitle} {`(${arr.length})`}</a>
-  </h1>
-  <ul className={styles.uList}>
-    {
-      arr.map((item, index) => 
-        <li className={styles.singleList} key={index}> {index + 1} -  {item}</li>
-        )
-    }
-  </ul>
-</div>
+export default ({pageTitle, arr, id='test'}) =>  {
+  const arrWithOutBr = arr.filter(loc => loc != '<br>')
+   return (<div>
+    <h1 id={id} className={styles.pageHeader}>
+      <a href='#'>{pageTitle} {`(${arrWithOutBr.length})`}</a>
+    </h1>
+    <ul className={styles.uList}>
+      {
+        arr.map((item, index) => {
+          return item === '<br>' ?
+          <li className={`${styles.singleList} ${styles.singleListWithBr}`} key={index}></li> :
+          <li className={styles.singleList} key={index}> {index + 1} -  {item}</li>
+
+        })
+      }
+    </ul>
+  </div>)  
+}
+
--- src/pages/detailloci.js
+++ src/pages/detailloci.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Loci from '../components/Loci.js'
+import LociLi from '../components/LociLi'
 import village from '../data/villageLociArray.js'
 import {
   fatepurHome,
@@ -17,36 +18,49 @@ import {
   dhakaMama,
   tarunHouse
 } from '../data/lociArray.js'
-export default () => 
-<div>
-  <h3>Table of Content(ToC)</h3>
-  <ul className='uList'>
-    <li className='singleList'> <a href='#fatepurHome'>Fatepur Home - {fatepurHome.length} </a> </li>
-    <li className='singleList'> <a href='#palashpurBasudev'>Palashpur Basudev Home - {palashpurBasudev.length} </a></li>
-    <li className='singleList'> <a href='#palashpurBiren'>Palashpur Biren Home - {palashpurBiren.length} </a> </li>
-    <li className='singleList'> <a href='#feniMess'>feniMess houses - {feniMess.length}</a> </li>
-    <li className='singleList'> <a href='#palashHome'>palashHome - {palashHome.length}</a> </li>
-    <li className='singleList'> <a href='#dhakaMess1'>dhakaMess1 - {dhakaMess1.length}</a> </li>
-    <li className='singleList'> <a href='#indiaMama'>indiaMama - {indiaMama.length}</a> </li>
-    <li className='singleList'> <a href='#palashMess'>palashMess - {palashMess.length}</a> </li>
-    <li className='singleList'> <a href='#dhakaMama'>dhakaMama houses - {dhakaMama.length}</a> </li>
-    <li className='singleList'> <a href='#tarunHouse'>tarunHouse houses - {tarunHouse.length}</a> </li>
-    <li className='singleList'> <a href='#banjaraHills'>banjaraHills - {banjaraHills.length}</a> </li>
-    <li className='singleList'> <a href='#village'>Village houses - {village.length}</a> </li>
-  </ul>
+// class LociLi extends React.Component {
+//   arrayCount = (arr) => arr.filter(loc => loc !='<br>').length
+//   render = () => <li className='singleList'> <a href={`#${this.props.id}`}>{this.props.pageTitle} - {this.arrayCount(this.props.arr)} </a> </li>
+// }
+class detailloci extends React.Component {
+  arrayCount = (arr) => arr.filter(loc => loc !='<br>').length
+  render () {
+    return (
+      <div>
+        <h3>Table of Content(ToC)</h3>
+        <ul className='uList'>
+          <LociLi id='fatepurHome' pageTitle="Fatepur Home" arr={fatepurHome} />
+          <LociLi id='palashpurBasudev' pageTitle="Palashpur Basudev Home" arr={palashpurBasudev} />
+          <LociLi id='palashpurBiren' pageTitle="Palashpur Biren Home" arr={palashpurBiren} />
+          <LociLi id='feniMess' pageTitle="feniMess houses" arr={feniMess} />
+          <LociLi id='palashHome' pageTitle="palash Home" arr={palashHome} />
+          <LociLi id='dhakaMess1' pageTitle="Dhaka mess main(1)" arr={dhakaMess1} />
+          <LociLi id='indiaMama' pageTitle="India mama Home" arr={indiaMama} />
+          <LociLi id='palashMess' pageTitle="Palash Mess" arr={palashMess} />
+          <LociLi id='dhakaMama' pageTitle="dhakaMama houses" arr={dhakaMama} />
+          <LociLi id='tarunHouse' pageTitle="tarunHouse houses" arr={tarunHouse} />
+          <LociLi id='banjaraHills' pageTitle="banjaraHills" arr={banjaraHills} />
+          <LociLi id='village' pageTitle="Village houses" arr={village} />
+        </ul>
 
-  <Loci id='fatepurHome' pageTitle="Fatepur Home" arr={fatepurHome} />
-  <Loci id='palashpurBasudev' pageTitle="Palashpur Basudev Home" arr={palashpurBasudev} />
-  <Loci id='palashpurBiren' pageTitle="Palashpur Biren Home" arr={palashpurBiren} />
-  <Loci id='feniMess' pageTitle="feniMess houses" arr={feniMess} />
-  <Loci id='palashHome' pageTitle="palash Home" arr={palashHome} />
-  <Loci id='dhakaMess1' pageTitle="Dhaka mess main(1)" arr={dhakaMess1} />
-  <Loci id='indiaMama' pageTitle="India mama Home" arr={indiaMama} />
-  <Loci id='palashMess' pageTitle="Palash Mess" arr={palashMess} />
-  <Loci id='dhakaMama' pageTitle="dhakaMama houses" arr={dhakaMama} />
-  <Loci id='tarunHouse' pageTitle="tarunHouse houses" arr={tarunHouse} />
-  <Loci id='banjaraHills' pageTitle="banjaraHills" arr={banjaraHills} />
-  <Loci id='village' pageTitle="Village houses" arr={village} />
-</div>
+        <Loci id='fatepurHome' pageTitle="Fatepur Home" arr={fatepurHome} />
+        <Loci id='palashpurBasudev' pageTitle="Palashpur Basudev Home" arr={palashpurBasudev} />
+        <Loci id='palashpurBiren' pageTitle="Palashpur Biren Home" arr={palashpurBiren} />
+        <Loci id='feniMess' pageTitle="feniMess houses" arr={feniMess} />
+        <Loci id='palashHome' pageTitle="palash Home" arr={palashHome} />
+        <Loci id='dhakaMess1' pageTitle="Dhaka mess main(1)" arr={dhakaMess1} />
+        <Loci id='indiaMama' pageTitle="India mama Home" arr={indiaMama} />
+        <Loci id='palashMess' pageTitle="Palash Mess" arr={palashMess} />
+        <Loci id='dhakaMama' pageTitle="dhakaMama houses" arr={dhakaMama} />
+        <Loci id='tarunHouse' pageTitle="tarunHouse houses" arr={tarunHouse} />
+        <Loci id='banjaraHills' pageTitle="banjaraHills" arr={banjaraHills} />
+        <Loci id='village' pageTitle="Village houses" arr={village} />
+      </div>
+
+
+      )
+  }
+}
+export default detailloci;
   
 
