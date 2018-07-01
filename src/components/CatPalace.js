c56372c14f1680065369c66ad7040ed3e6dc7cfc
polodev <polodev10@gmail.com>
2018-07-02 03:55:16 +0600

    added nook counting

--- src/components/CatPalace.js
+++ src/components/CatPalace.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import TitleNook from './TitleNook'
 
-export default ({arr, pageTitle, id}) =>
-  <div id={id}>
-    <h2 style={{textAlign: 'center', borderBottom: "2px solid salmon", marginTop: '2em'}}>
-      <a href="#">
-        {pageTitle} - ({arr.length})
-      </a>
-    </h2>
-    <ul>
-      {arr.map((item, index) => <TitleNook key={index} item={item} />)}
-    </ul>
-  </div>
+export default ({arr, pageTitle, id}) => {
+  const numberOfNooks = arr.map(item => item.nooks.length).reduce((cumulative, newItem) => cumulative += newItem);
+  return (
+    <div id={id}>
+      <h2 style={{textAlign: 'center', borderBottom: "2px solid salmon", marginTop: '2em'}}>
+        <a href="#">
+          {pageTitle} - (place - {arr.length}) - (nooks - {numberOfNooks})
+        </a>
+      </h2>
+      <ul>
+        {arr.map((item, index) => <TitleNook key={index} item={item} />)}
+      </ul>
+    </div>
+
+    )
+}
+
