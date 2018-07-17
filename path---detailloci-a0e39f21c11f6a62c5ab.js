01d1b07203854644232bf96ad4021c6c95f1ac38
polodev <polodev10@gmail.com>
2018-07-17 16:31:04 +0600

    Updates

--- path---detailloci-a0e39f21c11f6a62c5ab.js
+++ path---detailloci-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([77171638734287],{422:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([77171638734287],{450:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---detailloci-a0e39f21c11f6a62c5ab.js.map
--- path---dominichotel-a0e39f21c11f6a62c5ab.js
+++ path---dominichotel-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xfef536cff3df],{425:function(f,t){f.exports={pathContext:{}}}});
+webpackJsonp([0xfef536cff3df],{453:function(f,t){f.exports={pathContext:{}}}});
 //# sourceMappingURL=path---dominichotel-a0e39f21c11f6a62c5ab.js.map
